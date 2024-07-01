92d3d8b4ffd77653a38d608329a62a650092c8dc
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-01 17:16:06 -0300

    Courses components refactored

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Course } from './../model/course';
+import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+
+
+
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
 
+export class CoursesComponent {
+  courses:  Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.listAll();
+  }
+
+  ngOnInit(): void {
+
+  }
 }
